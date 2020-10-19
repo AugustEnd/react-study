8946b3598ee8a76daf51f5bbc478ed7ea7b23d49
yegaofeng <gaofeng.ye@mobilemd.cn>
2020-10-20 00:19:35 +0800

    2

--- react-demo/src/index.tsx
+++ react-demo/src/index.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from './App';
 function App() {
+    const [text, setText] = useState("L1");
+    useEffect(() => {
+        setTimeout(() => {
+            setText("LI11111");
+        }, 5000);
+    }, []);
     return (
-        <div>
-            i am
-            <span>KaSong</span>
+        <div className="parent">
+            <div className="children-class1">
+                DIV1
+                <li>{text}</li>
+                <li>LI1-1</li>
+                <li>LI1-2</li>
+            </div>
+            {/* <div className="children-class2">
+                DIV2
+                <li>LI2</li>
+            </div>
+            <div className="children-class3">
+                DIV3 <li>LI3</li>
+            </div>
+            <div className="children-class4">
+                DIV4 <li>LI4</li>
+            </div> */}
         </div>
     );
 }
